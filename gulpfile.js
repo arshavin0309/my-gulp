1a7ccc6e876ffd2bb69d21a915aff69ab72a4243
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-06-05 13:41:16 +0500

    добавил title для 404

--- gulpfile.js
+++ gulpfile.js
@@ -100,14 +100,25 @@ function watching() {
                 const filePath = path.join(__dirname, 'app', req.url === '/' ? 'index.html' : req.url);
 
                 if (!fs.existsSync(filePath)) {
-                    req.url = '/404.html';
+                    const notFoundPath = path.join(__dirname, 'app', '404.html');
+                    if (fs.existsSync(notFoundPath)) {
+                        let html = fs.readFileSync(notFoundPath, 'utf8');
+                        
+                        // Вставим кастомный <title> для 404
+                        html = html.replace(/<title>(.*?)<\/title>/i, '<title>Страница не найдена - 404</title>');
+
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end(html);
+                        return;
+                    }
                 }
 
                 return next();
             }
         },
-        ghostMode: false // false - отключена синхронизация между окнами, true - включена (клики, скролл, формы)
+        ghostMode: false
     });
+    
     watch(['app/scss/**/*.scss'], styles)
     watch(['app/images/src/**/*.*'], images)
     watch(['app/js/**/*.js', '!app/js/main.min.js',], scripts)
