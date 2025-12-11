cefbdaa7c8b803e816f70f31c8fd2b749b73681e
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-12-11 15:58:16 +0500

    закомментировал accordion

--- app/js/src/accordion.js
+++ app/js/src/accordion.js
@@ -1,8 +1,8 @@
 // !!!!!!!!!!!!переписать без jquery, jquery удален из сборки!!!!!!
 
-$(document).ready(function () {
-    $('.partner-tags__list > li').click(function (e) {
-        $(".partner-tags__list > li").not($(this)).removeClass("active").find(".answer").slideUp();
-        $(this).toggleClass("active").find(".answer").slideToggle();
-    });
-});
+// $(document).ready(function () {
+//     $('.partner-tags__list > li').click(function (e) {
+//         $(".partner-tags__list > li").not($(this)).removeClass("active").find(".answer").slideUp();
+//         $(this).toggleClass("active").find(".answer").slideToggle();
+//     });
+// });
